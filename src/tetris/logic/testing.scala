e4a3168e3c93927a111920b8c3d46202e6d2b975
Bilal el Ayachi <b.el.ayachi@student.vu.nl>
2024-08-16 20:53:33 +0200

    maze init

--- src/tetris/logic/testing.scala
+++ src/tetris/logic/testing.scala
@@ -8,23 +8,43 @@ class Maze(width: Int, height: Int) {
   val area: Int = width * height
   var mazeCells : ArrayBuffer[ArrayBuffer[Cell]] = ArrayBuffer[ArrayBuffer[Cell]]()
 
-  def initMaze(): Unit = {
-    for (i <- 0 until height) {
+  private def initMaze(): Unit = {
+
+    var roof = ArrayBuffer[Cell]().append(Cell(Point(0,0)).addWall('n').addWall('w'))
+    for (i <- 1 until width-1) roof.append(Cell(Point(i,0)).addWall('n'))
+    mazeCells.append(roof.append(Cell(Point(width-1,0)).addWall('n').addWall('e')))
+
+    for (i <- 1 until height-1) {
       var row: ArrayBuffer[Cell] = ArrayBuffer[Cell]()
       for (j <- 0 until width) {
-        row.append(new Cell(Point(i,j)))
+        if (j == 0) {
+          row.append(Cell(Point(j,i)).addWall('w'))
+        } else if (j == width-1) {
+          row.append(Cell(Point(j,i)).addWall('e'))
+        } else {
+          row.append(Cell(Point(j,i)))
+        }
       }
       mazeCells.append(row)
     }
+    var floor = ArrayBuffer[Cell]().append(Cell(Point(0,height-1)).addWall('s').addWall('w'))
+    for (l <- 1 until width-1) floor.append(Cell(Point(l,height-1)).addWall('s'))
+    mazeCells.append(floor.append(Cell(Point(width-1,height-1)).addWall('s').addWall('e')))
+
   }
 
   initMaze()
 
-  //Array[Array[Cell]]
 
-  class Cell(pos: Point) {
+  case class Cell(pos: Point) {
     val coordinate : Point = pos
-    val walls : Map[Char, Boolean] = Map[Char, Boolean]('u' -> true, 'd' -> true, 'l' -> true, 'r' -> true)
+    var walls : collection.mutable.Map[Char, Boolean] = collection.mutable.Map[Char, Boolean]('n' -> false, 's' -> false, 'w' -> false, 'e' -> false)
+
+    def addWall(wall: Char): Cell = {
+      walls(wall) = true
+      return this
+    }
+
   }
 
 }
